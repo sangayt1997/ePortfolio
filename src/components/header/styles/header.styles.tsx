a3ba7efb09b7cde00fd85c62f50f355fb0392ee7
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-02 14:08:03 +0600

    refactor(ePortfolio): header enhancement

--- src/components/header/styles/header.styles.tsx
+++ src/components/header/styles/header.styles.tsx
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import { Navbar } from "react-bootstrap";
 import { HashLink as Link } from "react-router-hash-link";
 
+export const CustomNavBar = styled(Navbar)`
+  box-shadow: rgba(17, 12, 46, 0.15) 0 48px 100px 0;
+`
 export const NavBrand = styled(Navbar.Brand)`
   font-size: 16px;
   font-weight: bold;
 `
-
 export const NavLink = styled(Link)`
   font-size: 12px;
   font-weight: bold;
